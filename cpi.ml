f3322f41f6a50ef65f4322392a877aa871dceee6
Eddy <ewgarci@gmail.com>
2013-10-20 23:21:14 -0400

    write code to file cpi_out.s

--- cpi.ml
+++ cpi.ml
@@ -3,8 +3,7 @@ open Printf
 
 let rec string_of_printf_args k = function
       | [] -> ""
-      | h :: t -> "mov r" ^ (string_of_int k) ^ ", #" ^ (string_of_expr h) ^ "\n" ^
-      string_of_printf_args (k+1) t
+      | h :: t -> "mov r" ^ (string_of_int k) ^ ", #" ^ (string_of_expr h) ^ "\n" ^ string_of_printf_args (k+1) t
 
 let rec code_expr = function
   Literal(x) -> "mov r0, #" ^ (string_of_int x) ^ "\n"
