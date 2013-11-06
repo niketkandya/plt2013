223f00f5bc2cd70c5884cebfa226da774f036ed8
Eddy <ewgarci@gmail.com>
2013-11-06 18:54:59 -0500

    added a test comment

--- cpi.ml
+++ cpi.ml
@@ -4,5 +4,5 @@ let program =
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
   Compile.translate program;;
-
+(* Test Comment *)
 let some = Execute.execute_prog  program
