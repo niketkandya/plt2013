9bc2a1a15114ff98933b425e6b679f9efd834f64
Eddy Garcia <ewgarci@gmail.com>
2013-12-17 23:19:25 -0500

    added directory for type checking tests

---
+++ tc_tests/intptr.cpi
@@ -0,0 +1,11 @@
+int fun(int b) {
+	return b;
+}
+int main()
+{
+	int *a;
+	int b;
+	a = &b;
+	*a = 5;
+	return b;
+}
---
+++ tc_tests/runtests.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+
+CPI_COMPILER="../cpi --stdout --stdin"
+
+passed_tests=0
+failed_tests=0
+
+
+
+# Takes two arguments
+#   $1, String to print
+#   $2 (optional), color. Possible colors are "red" and "green".
+#                       Otherwise, will default to bold
+# Will print to both console AND results file in /webserver/tests
+colorprint () {
+    bold="\033[1;1m"
+    red="\033[1;31m"
+    green="\033[1;32m"
+    default="\033[1;0m"
+
+    prefix=""
+    if [[ "$2" == "red" ]]; then
+        prefix=$red
+    elif [[ "$2" == "green" ]]; then
+        prefix=$green
+    else
+        prefix=$bold
+    fi
+
+    echo -e "$prefix$1$default"
+
+}
+
+
+# Clean
+rm -rf "out"
+mkdir -p "out"
+
+
+# Does test for each assembly file
+for file in *.cpifail; do
+    error=""
+    basename=${file%.*}
+    
+    colorprint "Testing: $basename"
+
+    # Corner case for bash glob suckiness
+    if [[ "$basename" == "*" ]]; then
+        echo "No Assembly files found. Exiting"
+        exit
+    fi
+
+    # Compile
+    $CPI_COMPILER < "$file" > "$basename.s"
+    if [[ $? != 0 ]]; then
+        error="cpi compile error"
+    fi
+
+    # Assemble and link our asm files
+    #as -o out/$basename.o $basename.s
+    #if [[ $? != 0 ]]; then
+    #    error="Assembly error (as)"
+    #fi
+
+    if [[ "$error" != "" ]]; then
+        let passed_tests++
+        colorprint "    Passed: $basename" "green"
+    else
+        let failed_tests++
+        colorprint "    Failed: $basename. $error" "red"
+        error=""
+    fi
+done
+
+# Does test for each assembly file
+for file in *.cpipass; do
+    error=""
+    basename=${file%.*}
+    
+    colorprint "Testing: $basename"
+
+    # Corner case for bash glob suckiness
+    if [[ "$basename" == "*" ]]; then
+        echo "No Assembly files found. Exiting"
+        exit
+    fi
+
+    # Compile
+    $CPI_COMPILER < "$file" > "$basename.s"
+    if [[ $? != 0 ]]; then
+        error="CPI COMPILE ERROR"
+    fi
+
+    # Assemble and link our asm files
+    #as -o out/$basename.o $basename.s
+    #if [[ $? != 0 ]]; then
+    #    error="Assembly error (as)"
+    #fi
+
+    if [[ "$error" != "" ]]; then
+        let failed_tests++
+        colorprint "    Failed: $basename. $error" "red"
+        error=""
+    else
+        let passed_tests++
+        colorprint "    Passed: $basename" "green"
+    fi
+done
+
+
+colorprint "\n\nTest results:"
+colorprint "Total Passed:$passed_tests" "green"
+colorprint "Total Failed:$failed_tests" "red"
